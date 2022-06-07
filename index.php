b25fca9490e24c112daa1311961501fd627b429b
Kunal Dixit <contactkunaldixit@gmail.com>
2022-06-08 02:48:52 +0800

    WIP for  View contacts Functional;ity

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ if ((!empty($_SESSION["user"]) && !empty($_SESSION["user"]["id"]))) {
 } else {
     $userId = 0;
     header("location:" . SITE_URL . "login.php");
+    exit();
 }
 ?>
 
@@ -30,32 +31,36 @@ if ((!empty($_SESSION["user"]) && !empty($_SESSION["user"]["id"]))) {
             $contactsRows = mysqli_num_rows($contactsResult);
             if ($contactsRows > 0) {
         ?>
-                <table class="table text-center">
-                    <thead>
-                        <tr>
-                            <th scope="col"></th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php while ($rows = mysqli_fetch_assoc($contactsResult)) {
+        <table class="table text-center">
+            <thead>
+                <tr>
+                    <th scope="col"></th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while ($rows = mysqli_fetch_assoc($contactsResult)) {
                         ?>
 
-                            <tr>
-                                <td class="align-middle"><img src="https://via.placeholder.com/50.png/09f/666" class="img-thumbnail img-list" /></td>
-                                <td class="align-middle"><?php echo $rows["first_name"] . " " . $rows["last_name"] ?></td>
-                                <td class="align-middle">
-                                    <a href="/contactbook/view.php?id=9" class="btn btn-success">View</a>
-                                    <a href="/contactbook/addcontact.php?id=9" class="btn btn-primary">Edit</a>
-                                    <a href="/contactbook/delete.php?id=9" class="btn btn-danger" onclick="return confirm(`Are you sure want to delete this contact?`)">Delete</a>
-                                </td>
-                            </tr>
-                        <?php } ?>
+                <tr>
+                    <td class="align-middle"><img src="https://via.placeholder.com/50.png/09f/666"
+                            class="img-thumbnail img-list" />
+                    </td>
+                    <td class="align-middle"><?php echo $rows["first_name"] . " " . $rows["last_name"] ?></td>
+                    <td class="align-middle">
+                        <a href="<?php echo SITE_URL . 'view.php?id=' . $rows["id"]; ?> "
+                            class="btn btn-success">View</a>
+                        <a href="/contactbook/addcontact.php?id=9" class="btn btn-primary">Edit</a>
+                        <a href="/contactbook/delete.php?id=9" class="btn btn-danger"
+                            onclick="return confirm(`Are you sure want to delete this contact?`)">Delete</a>
+                    </td>
+                </tr>
+                <?php } ?>
 
-                    </tbody>
-                </table>
-                <?php
+            </tbody>
+        </table>
+        <?php
                 include_once "./includes/config.php";
                 getPagination($contactsRows);
                 ?>
---
+++ view.php
@@ -0,0 +1,84 @@
+<?php
+ob_start();
+session_start();
+include_once "common/header.php";
+include_once "./includes/db.php";
+
+// Checking if the user is loggedIn:
+
+if (!empty($_SESSION["user"])) {
+    $userId = $_SESSION["user"]["id"];
+} else {
+    $userId = 0;
+    header("location:" . SITE_URL . "login.php");
+    exit();
+}
+
+$userId = (!empty($_SESSION["user"]) && !empty($_SESSION["user"]["id"])) ? $_SESSION["user"]["id"] : 0;
+$contactId = $_GET["id"];
+if (!empty($contactId) && is_numeric($contactId)) {
+    $conn = db_connect();
+    $contact_Id = mysqli_real_escape_string($conn, $contactId);
+    $sql = "SELECT * FROM `contacts` WHERE `id` = {$contact_Id}AND `owner_id` = {$userId}";
+    $sqlResult = mysqli_query($conn, $sql);
+    $rows = mysqli_num_rows($sqlResult);
+    if ($rows > 0) {
+        $contactResult = mysqli_fetch_assoc($sqlResult);
+    } else {
+        echo "Record doesnt Exists.";
+        exit();
+    }
+} else {
+    echo "Invalid contact ID";
+    exit();
+}
+
+
+?>
+
+<body>
+    <?php include_once "common/navigation.php" ?>
+
+    <main role="main" class="container">
+
+        <div class="row justify-content-center wrapper">
+            <div class="col-md-6">
+                <div class="card">
+                    <header class="card-header">
+                        <h4 class="card-title mt-2">Contact</h4>
+                    </header>
+                    <article class="card-body">
+                        <div class="container" id="profile">
+                            <div class="row">
+                                <div class="col-sm-6 col-md-4">
+                                    <img src="https://via.placeholder.com/50.png/09f/666" width="150"
+                                        class="img-thumbnail" />
+                                </div>
+                                <div class="col-sm-6 col-md-8">
+                                    <h4 class="text-primary">
+                                        <?php
+                                        echo $contactResult['first_name'] . " " . $contactResult['last_name']
+                                        ?>
+                                    </h4>
+                                    <p class="text-secondary">
+                                        <i class="fa fa-envelope-o" aria-hidden="true"></i> deepak@gmail.com<br />
+                                        <i class="fa fa-phone" aria-hidden="true"></i> 9776655444<br />
+                                        <i class="fa fa-map-marker" aria-hidden="true"></i> Delhi
+                                    </p>
+                                    <!-- Split button -->
+                                </div>
+                            </div>
+
+                        </div>
+                    </article>
+
+                </div>
+            </div>
+
+        </div>
+
+    </main>
+    <?php include_once "common/footer.php"; ?>
+</body>
+
+</html>
